5e1830834d4132f22447ec7e09418a86fcb31ce3
!            𝐷𝑎𝑟𝑘𝑠…! <alijanopalijanop0@gmail.com>
2025-01-02 20:36:14 +0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,15 @@
+const wait = require('wait')
+require('dotenv').config()
+const path = require('path')
+const Satxler = require(`./structures/Satxler.js`)
+const client = new Satxler()
+this.config = require(`${process.cwd()}/config.json`);
+(async () => {
+    await client.initializeMongoose()
+    await client.initializedata()
+    await wait(3000);
+    (await client.loadEvents()) - (await client.loadlogs())
+    await client.loadMain()
+    await client.login(this.config.TOKEN)
+
+})()
